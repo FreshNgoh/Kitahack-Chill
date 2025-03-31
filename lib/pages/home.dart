2d1a4a7cf1e405637c8089d0c85e2b6da0e296c6
FreshNgoh <jiankiat4486@gmail.com>
2025-04-01 00:53:28 +0800

    Navbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/components/bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Bar();
   }
 }
