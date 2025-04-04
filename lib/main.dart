e6b59e848fdb395c8b594912af6e3c659d08c3eb
FreshNgoh <jiankiat4486@gmail.com>
2025-04-04 23:00:20 +0800

    Add google restaurant suggestion

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/bloc/chat_bloc_bloc.dart';
+import 'package:flutter_application/bloc/google_map_bloc.dart';
 import 'package:flutter_application/components/bar.dart';
+import 'package:flutter_application/repos/google_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
@@ -12,8 +14,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => ChatBlocBloc(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => ChatBlocBloc()),
+        BlocProvider(create: (context) => GoogleMapBloc(RestaurantRepo())),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter JK',
