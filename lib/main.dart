916a8e1b18502870e27e1ba6743455080c5b0dab
yongliang <chan.yongliang7406@gmail.com>
2025-04-06 11:08:22 +0800

    android version edit

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
           fontFamily: "Poppins",
           scaffoldBackgroundColor: Colors.white,
         ),
-        home: AuthWrapper(),
+        home: Bar(),
       ),
     );
   }
